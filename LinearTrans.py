d4aeb7206236aea0e10a825d2483bb73f7b38627
realdy <realdy@umich.edu>
2020-04-16 22:56:26 -0400

    Changed plot labels to be more descriptive

---
+++ LinearTrans.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import sklearn as sk
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+import matplotlib.patches as mpatches
+
+def initialize():
+    path = './csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+    row_drop = ['Province/State', 'Country/Region', 'Lat', 'Long']
+    file = pd.read_csv(path)
+    country_names = (file['Country/Region'])
+    trans_file = file.transpose()
+    trans_file.columns = country_names
+    trans_file = trans_file.drop(row_drop)
+    trans_file = trans_file.groupby(lambda x:x, axis=1).sum()
+    sum_column = trans_file.sum(axis=1)
+    trans_file['Total_Count'] = sum_column
+    day_count = []
+    for x in range(len(trans_file)):
+        day_count.append(x)
+    trans_file['Day_Count'] = day_count
+    print(trans_file)
+    return trans_file, country_names
+
+def create_lin_reg_plot(X, y, X_test, y_pred, country_name, days, intercept, coef):
+    plt.scatter(X, y, color='gray')
+    plt.plot(X_test, y_pred, color = 'red', linewidth=2)
+    plt.title('Daily Covid-19 Cases in ' + country_name + ' Across ' + days + ' Days')
+    plt.xlabel('Day Count')
+    plt.ylabel('Covid-19 Cases')
+    red_patch = mpatches.Patch(color='red', label= 'y = ' + str(round(intercept[0], 3)) + ' + ' + str(round(coef[0][0], 3)) + 'X')
+    plt.legend(handles=[red_patch])
+    plt.show()
+
+def create_linear_regression(df, country_name):
+    print(country_name)
+    X = df['Day_Count'].values.reshape(-1, 1)
+    y = df[country_name].values.reshape(-1, 1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .2, random_state=0)
+    regressor = LinearRegression()
+    regressor.fit(X_train, y_train)
+    print(regressor.intercept_)
+    print(regressor.coef_)
+    y_pred = regressor.predict(X_test)
+    res = pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()})
+    res.to_csv('.\Results\\' + country_name + 'Results.csv')
+    create_lin_reg_plot(X, y, X_test, y_pred, country_name, str(len(df)), regressor.intercept_, regressor.coef_)
+
+def create_exp_regression(df, country_name):
+    X = df['Day_Count'].values.reshape(-1, 1)
+    y = df[country_name].values.reshape(-1, 1)
+    np.polyfit(np.log(X), y, 1)
+
+def main():
+    df, country_names = initialize()
+    #df.to_csv('Test.csv')
+    create_linear_regression(df, "Total_Count")
+    create_linear_regression(df, "US")
+    create_exp_regression(df, "Total_Count")
+    #for name in country_names:
+     #   create_linear_regression(df, name)
+      #  exit()
+        #create_plot(df, name)
+
+main()
